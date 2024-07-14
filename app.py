0f80345efba2509a07352a071267d9a55e9943d5
MoonXDevloper <ak3515400@gmail.com>
2024-07-14 07:08:37 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_wprld():
+    return 'GreyMatters'
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+werkzeug.urls import url_quote
